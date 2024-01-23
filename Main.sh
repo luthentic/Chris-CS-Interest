1d61bc63ee1c67adf33a04a441cd6c6b16a7ab24
SEWOONG LEE <dltpdnd1234@gmail.com>
2024-01-23 17:06:19 -0500

    Test Commit2

--- Main.sh
+++ Main.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
-read -p "Enter a number and I will check if its odd or even " mynumber
-if [ $((mynumber%2)) -eq 0 ]
-then
-echo "Your number is even"
-else
-echo "Your number is odd."
-fi
+
+echo "Enter the user name: "  
+read first_name  
+echo "The Current User Name is $first_name"  
+echo  
+echo "Enter other users'names: "  
+read name1 name2 name3  
+echo "$name1, $name2, $name3 are the other users."  
